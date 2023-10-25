669fe9390c5a5ab4b4ebc1932a1b6777a286b2ce
w0735817 <jonathan.freeman@selu.edu>
2023-10-25 15:11:57 -0500

    added quotient

--- CircleWithException.java
+++ CircleWithException.java
@@ -1,5 +1,7 @@
 package chapter12;
 
+import java.sql.SQLOutput;
+
 public class CircleWithException {
   /** The radius of the circle */
   private double radius;
@@ -7,6 +9,17 @@ public class CircleWithException {
   /** The number of the objects created */
   private static int numberOfObjects = 0;
 
+  public static void main(String[] args) {
+    try {
+      CircleWithException circy = new CircleWithException(10);
+      CircleWithException critical = new CircleWithException(0);
+      CircleWithException rings = new CircleWithException(-13);
+    }
+    catch(IllegalArgumentException circyL){
+      System.out.println(circyL);
+    }
+    System.out.println("number of objects created: " + CircleWithException.getNumberOfObjects());
+  }
   /** Construct a circle with radius 1 */
   public CircleWithException() {
     this(1.0);
